b63737ab340229ff1c7e63ec2d5765fcb30c7a6f
laurajanefaulds <laurajanefaulds@gmail.com>
2023-06-05 13:34:12 -0400

    git ignore

--- ljf_code/api_keys.py
+++ ljf_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "efc26ec6c0d57b8a5eda8ed3a9c2dcb5"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "48080a1ffd604d9dbdd302b926cd39d3"
